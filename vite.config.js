c699251c55aaa092aef4a0f186a170173e749a97
amcoddieng <dieng0097@gmail.com>
2026-06-17 16:05:06 +0000

    connection render

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://capadmis.onrender.com',
         changeOrigin: true,
       },
     },
