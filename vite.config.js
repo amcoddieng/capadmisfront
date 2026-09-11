07ce0aaa25bd4f3011d905efa44a1309ab2d9076
amcoddieng <dieng0097@gmail.com>
2026-09-11 12:55:27 +0000

    ajout du plugin vite-plugin-css-injected-by-js pour injecter le CSS directement dans le JavaScript

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 // maintenent on utilise le server en local
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   server: {
     proxy: {
       '/api': {
